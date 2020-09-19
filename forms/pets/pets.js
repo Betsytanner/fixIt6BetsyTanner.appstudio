fe1532b8f7fbd75091720d389b43ab1beb066d0f
Betsytanner <64242709+Betsytanner@users.noreply.github.com>
2020-09-19 14:36:20 -0500

    Initial commit

---
+++ forms/pets/pets.js
@@ -0,0 +1,30 @@
+const petNames = ['Darcy', 'Jack', 'Jerry', 'Marty', 'Vito', 'Vinny', 'Riley', 'Saddie', 'Marge', 'Sam'];
+const withMarmaduke = petNames.push('Marmaduke');
+const coolArray = withMarmaduke.splice(3, 2);
+//prompt for vinny
+prompt("Would you like to remove Vinny from the selection?")
+if 'yes':
+withMarmaduke.delete(6)
+prompt('the remaining array data are:' withMarmaduke);
+else: 
+  break 
+console.log(withMarmaduke)
+//prompt for Marty 
+prompt("Would you like to remove Marty from the selection?")
+if 'yes':
+withMarmaduke.delete(4)
+prompt('the remaining array data are:' withMarmaduke);
+else: 
+  break 
+console.log(withMarmaduke)
+  //> add "Nancy" to the front of the array.
+prompt("Would you like to add Nancy from the selection?")
+if 'yes':
+withMarmaduke.unshift('Nancy)
+prompt('the remaining array data are:' withMarmaduke);
+else: 
+  break 
+  console.log(withMarmaduke)
+//add their name to the end of the array.
+
+
